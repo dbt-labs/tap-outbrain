cc43da838ada60e815b0f6c9efd924a963a4b702
Connor McArthur <cmcarthur@rjmetrics.com>
2017-02-13 10:57:42 -0500

    pulling complete data set

--- README.md
+++ README.md
@@ -6,7 +6,10 @@ Author: Connor McArthur (connor@fishtownanalytics.com)
 
 This repository contains two configuration files that need to be copied and filled out before running:
 
-- `config.json.example`: copy to `config.json` in the repo root. Contains the `access_token`, which is the Outbrain access token / API key. Also contains the `account_id`, aka the Marketer ID (unique to each account) in Outbrain. Looks like `00f4b02153ee75f3c9dc4fc128ab041962`.
+- `config.json.example`: copy to `config.json` in the repo root. Contains:
+  - `account_id`, aka the Marketer ID (unique to each account) in Outbrain. Looks like `00f4b02153ee75f3c9dc4fc128ab041962`.
+  - `username`, the Outbrain username used to generate an Amplify API token.
+  - `password`, the Outbrain password to go along with `username`.
 
 - `persist.json.example`: copy to `persist.json` in the repo root. Contains the configuration for the Stitch persister.
 
@@ -28,10 +31,9 @@ docker -v "$(pwd)":/usr/src/tap-outbrain run <image-id>
 
 ### Gotchas
 
-- Outbrain only allows 5 requests per minute to the performance reporting API, which makes this integration potentially slow.
-- There's a 30 day expiration on every access token, meaning this integration has to refresh its own token. This is not yet implemented but could be done using the state file.
-
+- Outbrain only allows two calls to the `/login` API per hour. This integration calls that API on every run to generate a new access token. This means that this integration cannot be run more frequently than twice per hour. The access token could be stored in the state file with a timestamp, but at present secure state file storage is not implemented.
+- Campaign pagination is not implemented -- this integration pulls incomplete data if more than 100 campaigns exist.
 
 ---
 
-Copyright &copy; 2017 Stitch
+Copyright &copy; 2017 Fishtown Analytics
--- tap_outbrain.py
+++ tap_outbrain.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python3
 
 import argparse
+import base64
 import datetime
 import dateutil.parser
 import json
 import os
 import sys
+import time
 
 import backoff
 import requests
@@ -17,42 +19,62 @@ logger = stitchstream.get_logger()
 
 BASE_URL = 'https://api.outbrain.com/amplify/v0.1'
 
-BASE_URL = 'http://private-anon-8db7159ad4-amplifyv01.apiary-mock.com'
-
 DEFAULT_STATE = {
     'campaign_performance': {}
 }
 
-DEFAULT_START_DATE = '2017-01-01'
+DEFAULT_START_DATE = '2016-01-01'
 
-REQUEST_COUNT = 0
+
+# TODO
+#  - campaign pagination -- not needed if there are fewer than 100 campaigns
 
 def giveup(error):
     response = error.response
     return not (response.status_code == 429 or
                 response.status_code >= 500)
 
-@backoff.on_exception(backoff.expo,
+@backoff.on_exception(backoff.constant,
                       (requests.exceptions.RequestException),
+                      jitter=backoff.random_jitter,
                       max_tries=5,
                       giveup=giveup,
-                      factor=2,
-                      base=15)
+                      interval=30)
 def request(url, access_token, params={}):
-    global REQUEST_COUNT
+    logger.info("Making request: GET {} {}".format(url, params))
 
-    logger.debug("Making request: GET {} {}".format(url, params))
+    try:
+        response = requests.get(
+            url,
+            headers={'OB-TOKEN-V1': access_token},
+            params=params)
+    except e:
+        logger.exception(e)
 
-    response = requests.get(
-        url,
-        headers={'OB-TOKEN-V1': access_token},
-        params=params)
+    logger.info("Got response code: {}".format(response.status_code))
 
-    logger.debug("Got response code: {}".format(response.status_code))
-
-    REQUEST_COUNT += 1
     response.raise_for_status()
     return response
+
+
+def generate_token(username, password):
+    logger.info("Generating new token using basic auth.")
+
+    encoded = base64.b64encode(bytes('{}:{}'.format(username, password),
+                                     'utf-8')) \
+                    .decode('utf-8')
+
+    try:
+        response = requests.get(
+            '{}/login'.format(BASE_URL),
+            headers={'Authorization': 'Basic {}'.format(encoded)})
+    except e:
+        logger.exception(e)
+        raise e
+
+    logger.info("Got response code: {}".format(response.status_code))
+
+    return response.json().get('OB-TOKEN-V1')
 
 
 def parse_datetime(datetime):
@@ -63,64 +85,104 @@ def parse_datetime(datetime):
     return dt.isoformat('T') + 'Z'
 
 
-def parse_campaign_performance(item, header, campaign_id):
+def parse_campaign_performance(result, campaign_id):
+    metrics = result.get('metrics', {})
+    metadata = result.get('metadata', {})
+
     return {
         'campaignId': campaign_id,
-        'fromDate': item.get('metadata', {}).get('fromDate'),
-        'impressions': item.get('metrics', {}).get('impressions', 0),
-        'clicks': item.get('metrics', {}).get('clicks', 0),
-        'ctr': item.get('metrics', {}).get('ctr', 0.0),
-        'spend': item.get('metrics', {}).get('spend', 0.0),
-        'ecpc': item.get('metrics', {}).get('ecpc', 0.0),
-        'conversions': item.get('metrics', {}).get('conversions', 0),
-        'conversionRate': item.get('metrics', {}).get('conversionRate', 0.0),
-        'cpa': item.get('metrics', {}).get('cpa', 0.0),
+        'fromDate': metadata.get('fromDate'),
+        'impressions': metrics.get('impressions', 0),
+        'clicks': metrics.get('clicks', 0),
+        'ctr': metrics.get('ctr', 0.0),
+        'spend': metrics.get('spend', 0.0),
+        'ecpc': metrics.get('ecpc', 0.0),
+        'conversions': metrics.get('conversions', 0),
+        'conversionRate': metrics.get('conversionRate', 0.0),
+        'cpa': metrics.get('cpa', 0.0),
     }
 
+
+def get_date_ranges(start, end, interval_in_days):
+    if start > end:
+        return []
+
+    to_return = []
+    interval_start = start
+
+    while interval_start < end:
+        to_return.append({
+            'from_date': interval_start,
+            'to_date': min(end,
+                           (interval_start + datetime.timedelta(
+                               days=interval_in_days-1)))
+        })
+
+        interval_start = interval_start + datetime.timedelta(
+            days=interval_in_days)
+
+    return to_return
 
 def sync_campaign_performance(state, access_token, account_id, campaign_id):
-    has_more = True
-    from_date = state.get('campaign_performance', {}) \
-                     .get(campaign_id, DEFAULT_START_DATE)
-    params = {
-        'campaignId': campaign_id,
-        'from': from_date,
-        'sort': '-from',
-        'breakdown': 'daily',
-        'limit': 100,
-    }
+    # sync 2 days before last saved date, or DEFAULT_START_DATE
+    from_date = datetime.datetime.strptime(
+        state.get('campaign_performance', {})
+             .get(campaign_id, DEFAULT_START_DATE),
+        '%Y-%m-%d').date() - datetime.timedelta(days=2)
 
-    while has_more:
+    to_date = datetime.date.today()
+
+    date_ranges = get_date_ranges(from_date, to_date, interval_in_days=100)
+
+    last_request_start = None
+
+    for date_range in date_ranges:
+        logger.info(
+            'Pulling campaign performance for id {} from {} to {}'
+            .format(campaign_id,
+                    date_range.get('from_date'),
+                    date_range.get('to_date')))
+
+        last_request_start = time.time()
         response = request(
             '{}/reports/marketers/{}/periodic'.format(BASE_URL, account_id),
             access_token,
-            params)
+            {
+                'campaignId': campaign_id,
+                'from': date_range.get('from_date'),
+                'to': date_range.get('to_date'),
+                'breakdown': 'daily',
+                'limit': 100,
+                'sort': '+fromDate',
+            })
+        last_request_end = time.time()
 
-        logger.info(response.text)
-
-        logger.info('Pulling campaign performance from {}'.format(from_date))
-        response_body = response.json()
-
-        header = response_body.get('metadata')
-        items = response_body.get('metrics')
+        logger.info('Done in {} sec'.format(time.time() - last_request_start))
 
         campaign_performance = [
-            parse_campaign_performance(item, header, campaign_id)
-            for item in items]
+            parse_campaign_performance(
+                result, campaign_id)
+            for result in response.json().get('results')]
 
         stitchstream.write_records('campaign_performance',
                                    campaign_performance)
 
-        last_record = logger.info(campaign_performance[-1])
-        logger.info(last_record)
-
+        last_record = campaign_performance[-1]
         new_from_date = last_record.get('fromDate')
+
         state['campaign_performance'][campaign_id] = new_from_date
         stitchstream.write_state(state)
+
         from_date = new_from_date
 
-        logger.info(response)
-        has_more = False
+        if last_request_start is not None and \
+           time.time() - last_request_end < 15:
+            to_sleep = 15 - (time.time() - last_request_end)
+            logger.info(
+                'Limiting to 4 requests per minute. Sleeping {} sec '
+                'before making the next reporting request.'
+                .format(to_sleep))
+            time.sleep(to_sleep)
 
 
 def parse_campaign(campaign):
@@ -138,14 +200,25 @@ def sync_campaigns(state, access_token, account_id):
     has_more = True
 
     while has_more:
+        logger.info('Syncing campaigns.')
+
+        start = time.time()
         response = request(
             '{}/marketers/{}/campaigns'.format(BASE_URL, account_id),
             access_token, params)
 
-        response_body = response.json()
-        campaigns = response_body.get('campaigns', [])
+        campaigns = [parse_campaign(campaign) for campaign
+                     in response.json().get('campaigns', [])]
 
-        campaigns = [parse_campaign(campaign) for campaign in campaigns]
+        stitchstream.write_records('campaigns', campaigns)
+
+        logger.info('Done in {} sec'.format(time.time() - start))
+
+        logger.info(
+            'Syncing campaign performance for {} campaigns.'
+            .format(len(campaigns)))
+
+        campaigns_done = 0
 
         for campaign in campaigns:
             sync_campaign_performance(state,
@@ -153,10 +226,15 @@ def sync_campaigns(state, access_token, account_id):
                                       account_id,
                                       campaign.get('id'))
 
-        stitchstream.write_records('campaigns', campaigns)
+            campaigns_done = campaigns_done + 1
 
-        logger.info(response)
+            logger.info(
+                '{}/{} campaigns synced.'
+                .format(campaigns_done, len(campaigns)))
+
         has_more = False
+
+    logger.info('Done!')
 
 
 def do_sync(args):
@@ -165,15 +243,26 @@ def do_sync(args):
     with open(args.config) as config_file:
         config = json.load(config_file)
 
-    access_token = config.get('access_token')
+    username = config.get('username')
+    password = config.get('password')
     account_id = config.get('account_id')
 
-    if access_token is None:
-        logger.fatal("Missing `access_token`.")
+    if username is None:
+        logger.fatal("Missing `username`.")
+        raise RuntimeError
+
+    if password is None:
+        logger.fatal("Missing `password`.")
         raise RuntimeError
 
     if account_id is None:
         logger.fatal("Missing `account_id`.")
+        raise RuntimeError
+
+    access_token = generate_token(username, password)
+
+    if access_token is None:
+        logger.fatal("Failed to generate a new access token.")
         raise RuntimeError
 
     stitchstream.write_schema('campaigns', schemas.campaign)
@@ -183,34 +272,17 @@ def do_sync(args):
     sync_campaigns(state, access_token, account_id)
 
 
-def do_check(args):
-    pass
-
-
 def main():
     parser = argparse.ArgumentParser()
 
-    subparsers = parser.add_subparsers()
-
-    parser_check = subparsers.add_parser('check')
-    parser_check.set_defaults(func=do_check)
-
-    parser_sync = subparsers.add_parser('sync')
-    parser_sync.set_defaults(func=do_sync)
-
-    for subparser in [parser_check, parser_sync]:
-        subparser.add_argument(
-            '-c', '--config', help='Config file', required=True)
-        subparser.add_argument(
-            '-s', '--state', help='State file')
+    parser.add_argument(
+        '-c', '--config', help='Config file', required=True)
+    parser.add_argument(
+        '-s', '--state', help='State file')
 
     args = parser.parse_args()
 
-    if 'func' in args:
-        args.func(args)
-    else:
-        parser.print_help()
-        exit(1)
+    do_sync(args)
 
 if __name__ == '__main__':
     main()
