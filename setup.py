8b98c9a652f02898acf33c5e413bb6fcac9af8af
Joshua Karstendick <jkarstendick@rjmetrics.com>
2017-03-14 14:41:25 -0400

    bump version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 import os.path
 
 setup(name='tap-outbrain',
-      version='0.1.8',
+      version='0.1.9',
       description='Singer.io tap for extracting data from the Outbrain API',
       author='Fishtown Analytics',
       url='http://singer.io',
