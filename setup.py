2bb37d8613c77bb184154a43cb77366bb86c38b6
Mike DeLaurentis <miked@stitchdata.com>
2017-09-28 15:51:32 -0400

    0.1.10

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 import os.path
 
 setup(name='tap-outbrain',
-      version='0.1.9',
+      version='0.1.10',
       description='Singer.io tap for extracting data from the Outbrain API',
       author='Fishtown Analytics',
       url='http://singer.io',
